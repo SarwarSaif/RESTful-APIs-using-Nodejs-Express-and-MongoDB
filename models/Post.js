3a930a127607523541c16c2778b2e5902e1543eb
Sarwar Saif <saifmu6@gmail.com>
2019-11-12 22:05:40 +0600

    Created Model for Posts using Mongoose

---
+++ models/Post.js
@@ -0,0 +1,22 @@
+// Import Mongoose 
+const mongoose = require('mongoose');
+
+
+// Create Schema 
+const PostSchema = mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: String,
+        default: Date.now
+    },
+})
+
+// Export The Mongoose Model
+module.exports = mongoose.model('Posts', PostSchema);
--- routes/posts.js
+++ routes/posts.js
@@ -1,6 +1,6 @@
 const express = require('express');
-
 const router = express.Router();
+const Post = require('../models/Post');
 
 // Post Route
 // Don't Need To Add /posts as the middleware will link it
@@ -8,6 +8,10 @@ router.get('/', (req, res) => {
     res.send('We are on posts');
 })
 
+// Use the post model
+router.post('/', (req, res)  => {
+    console.log(req.body);
+});
 
 // Exporting Router To Make It Visible To Other Files
 module.exports = router;
