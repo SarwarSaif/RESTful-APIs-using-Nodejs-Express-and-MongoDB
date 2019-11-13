1d31c562f66a4dd29c0c76ce793aef563ed24bfc
Sarwar Saif <saifmu6@gmail.com>
2019-11-13 21:29:41 +0600

    Converted post route into async/await

--- routes/posts.js
+++ routes/posts.js
@@ -8,20 +8,39 @@ router.get('/', (req, res) => {
     res.send('We are on posts');
 })
 
-// Use the post model
-router.post('/', (req, res)  => {
+// Use the post model 
+// General way of creating post route
+// router.post('/', (req, res)  => {
+//     const post = new Post({
+//         title: req.body.title,
+//         description: req.body.description
+//     });
+
+//     //save the post in the Database
+//     post.save() // Returns a promise
+//         .then(data => {
+//             res.json(data);
+//         })
+//         .catch(err => {
+//             res.json({ message: 'Post Creation Falied!' });
+//         });
+
+// });
+// Convert this function into "Async/await"
+// to work with the promises more comfortably
+router.post('/', async (req, res)  => {
     const post = new Post({
         title: req.body.title,
         description: req.body.description
     });
 
-    post.save() // Returns a promise
-        .then(data => {
-            res.json(data);
-        })
-        .catch(err => {
-            res.json({ message: 'Post Creation Falied!' });
-        });
+    try {
+        //save the post in the Database in async way
+        const savedPost = await post.save(); // Returns a promise
+        res.json(savedPost);
+    } catch (err) {
+        res.json({ message: err });
+    }
 
 });
 
